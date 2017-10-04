fe1ca4b9c2f9367093213f2d8b1ada1b804cf487
betalantz <betalantz@gmail.com>
2017-10-03 23:54:36 -0700

    added Player class, refactored Deck class so that Player can inherit

--- Deck of Cards/cards.js
+++ Deck of Cards/cards.js
@@ -8,17 +8,21 @@ class Card{
 
 // Deck class
 // It will have cards in it, shuffle, deal card, reset
-function Deck(){
-    this.cards = [];
-    let ranks = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Page", "Knight", "King", "Queen"];
-    let suits = ["Wands", "Swords", "Pentacles", "Cups"];
-
-    for(let suit of suits){
-        for(let rank of ranks){
-            this.cards.push(new Card(suit, rank))
+class Deck {
+    constructor(){
+        this.cards = [];
+        this.ranks = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Page", "Knight", "King", "Queen"];
+        this.suits = ["Wands", "Swords", "Pentacles", "Cups"];
+    }
+    makeDeck(){
+    
+        for(let suit of this.suits){
+            for(let rank of this.ranks){
+                this.cards.push(new Card(suit, rank))
+            }
         }
     }
-    this.shuffle = () => {
+    shuffle() {
         for(let i=0; i<this.cards.length; i++){
             let random_index = Math.floor(Math.random()*this.cards.length);
             let temp = this.cards[i];
@@ -26,19 +30,42 @@ function Deck(){
             this.cards[random_index] = temp;
         }
     }
-    this.deal = () => {
+    deal() {
         return this.cards.pop();
     }
-    this.reset = () => {
-        this.cards = [];
+    reset () {
+        this.cards = []
+        this.makeDeck();
     }
-    // this.reset();
+
+}
+class Player extends Deck {
+    constructor(name){
+        super()
+        this.name = name
+        this.hand = []
+    }
+    makeHand() {
+        super.reset()
+        super.shuffle()
+        for(let d=0; d<4; d++){
+            let draw = super.deal()
+            this.hand.push(draw)
+        }
+    }
+    discard(chosen) {
+        // return this.hand.splice(chosen, 1)
+        return this.hand.pop()
+    }
 }
 
 let my_deck = new Deck()
+my_deck.makeDeck()
 my_deck.shuffle()
 console.log(my_deck.cards)
 
-for(let d=0; d<4; d++){
-    console.log(my_deck.deal());
-}
+let player1 = new Player("Scotty")
+player1.makeHand()
+console.log(player1.hand)
+player1.discard(1)
+console.log(player1.hand)
