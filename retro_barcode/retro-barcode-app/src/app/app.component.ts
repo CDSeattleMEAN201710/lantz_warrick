d71eec120b0ad0ad624f91cff4d052a07be948e2
betalantz <betalantz@gmail.com>
2017-10-12 16:24:10 -0700

    resolved errors by generating boxes array OnInit

--- retro_barcode/retro-barcode-app/src/app/app.component.ts
+++ retro_barcode/retro-barcode-app/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  title: string = "Retro Barcode Generator"
+  colors: Array<string> = ['aqua', 'aquamarine', 'azure', 'lavender', 'cyan', 'chartreuse', 'MediumSeaGreen', 'MediumSpringGreen', 'RebeccaPurple', 'LightSteelBlue', 'HoneyDew', 'HotPink', 'DodgerBlue']
+  boxes: Array<any> = []
+
+  ngOnInit(){
+    for (let x = 0; x < 10; x++) {
+      this.boxes.push(this.randomColor())
+    }
+  }
+  randomColor () {
+    let range = this.colors.length-1
+    let color = this.colors[(Math.floor(Math.random()*range))+1]
+    return color
+  }
 }
