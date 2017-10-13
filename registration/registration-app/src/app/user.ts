ee874fa4479a546811d139ac7b0b0f49d6939cc7
betalantz <betalantz@gmail.com>
2017-10-13 14:17:31 -0700

    email validation working, expanding validations

--- registration/registration-app/src/app/user.ts
+++ registration/registration-app/src/app/user.ts
@@ -1,10 +1,15 @@
 export class User {
     constructor(
         public id: number = null,
-        public first_name: string = "",
-        public last_name: string = "",
+        public firstName: string = "",
+        public lastName: string = "",
         public email: string = "",
         public password: string = "",
+        public address: string = "",
+        public apt: string = "",
+        public city: string = "",
+        public state: string = "",
+        public lucky: string = "",
         public created_at: Date = new Date(),
         public updated_at: Date = new Date()
       ){}
