1797928d7b25030506dc74211a499e67a7071003
betalantz <betalantz@gmail.com>
2017-10-12 23:40:03 -0700

    app set-up, User class defined, form HTML

---
+++ registration/registration-app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { User } from './user'
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  user = new User();
+  users = [];
+  onSubmit(){
+    this.users.push(this.user);
+    this.user = new User();
+  }
+}
